a54a85ce0f09afcf301ac6b09e96573eaaa914db
HenrikSDU <117083964+HenrikSDU@users.noreply.github.com>
2022-11-26 17:09:11 +0100

    Screencommunication

--- src/main.c
+++ src/main.c
@@ -14,11 +14,19 @@
 
 volatile unsigned long int timer = 0, counter = 0; //timer: variable for the time; counter: counter to count timeroverflows
 volatile bool car_move_flag = false; //variable to indicate whether the car is moving
+
 char acceleration_index(double, double); //function for checking the state of acceleration
 char acceleration_flag = 0; //variable for indicating the state of aceleration
+
 double seconds, speed = 0, prev_speed = 0, eigthcircumference = 0.02589182;
 
 void initialize(void);//function for initializing the timer and interrupts
+
+ISR(USART_RX_vect){
+
+
+
+}
 
 ISR(TIMER1_CAPT_vect){
     timer=ICR1+65535*counter;
