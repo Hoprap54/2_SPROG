e01b9cfe73f4947a07470545f7d6a56ed0962e44
Borg-Unimatrix-01 <cbc@sg8.dk>
2023-03-24 15:45:47 +0100

    PWM stepper motor for microstepping
    Still W.I.P.

---
+++ PWM stepper motor/src/subfunctions.c
@@ -0,0 +1,14 @@
+#define F_CPU 16000000UL //needs to be defined for the delay functions to work.
+#define BAUD 9600
+#define NUMBER_STRING 1001
+//#define CIRCUMFERENCE 0.02589182
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <util/delay.h> //here the delay functions are found
+#include "usart.h"
+#include "subfunctions.h"
+#include <stdbool.h>
