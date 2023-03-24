4b1342c213188920c7491dfc5ad3a936cea68025
Hoprap <benitopmwk@gmail.com>
2023-03-24 10:42:56 +0100

    Update bipolar motor conf

--- src/main.c
+++ src/main.c
@@ -6,6 +6,8 @@
 *   D9 -> In2
 *   D10 -> In3
 *   D11 -> In4
+*
+*   Step advance .. m
 */
 
 #include <stdio.h>
@@ -22,16 +24,16 @@ int main(void) {
     /* Declaration of I/O Pins */
     DDRD = 0xF0;    // Set Port D as input for the buttons 0b1111 0000
     PORTD = 0x3F;   // Activate internall pull
-    DDRB = 0xFF;    // Set Port B as output for the LN298N 0b1111 0000
+    DDRB = 0xFF;    // Set Port B as output for the LN298N 0b1111 1111
 
     /* Declare variables */
     
     while (1){
         //button_state = PIND;
-        switch (PIND)
-        {
+        switch (PIND){
         case b0:
-            moveF();            
+            moveF();
+            _delay_ms(10);
             break;
         
         case b1:
@@ -46,19 +48,19 @@ int main(void) {
 
 void moveF(){
     int i;
-    char pos[4] = {0b0101, 0b0110, 0b1010, 0b1001};
-    for(int j = 0; j < 51 ; j++){
+    char pos[4] = {0b0001, 0b0100, 0b1000, 0b0010};
+    for(int j = 0; j < 50 ; j++){
         for(i = 0; i < 4 ; i++){
             PORTB = pos[i];
-            _delay_ms(5);
+            _delay_ms(30);
         }
     }
 }
 void moveB(){
     int i;
-    char pos[4] = {0b0101, 0b0110, 0b1010, 0b1001};
+    char pos[4] = {0b0001, 0b0100, 0b1000, 0b0010};
     for(i = 3; i >= 0 ; i--){
         PORTB = pos[i];
-        _delay_ms(5);
+        _delay_ms(30);
     }
 }
