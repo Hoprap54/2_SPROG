bbe1303c2ab92ade0244eec2b83d15237c5b375d
Hoprap <benitopmwk@gmail.com>
2023-03-31 13:27:16 +0200

    Function to move distance

--- src/main.c
+++ src/main.c
@@ -13,54 +13,83 @@
 #include <stdio.h>
 #include <avr/io.h>
 #include <util/delay.h>
+#include "usart.h"
 
 #define b0 0x3b
 #define b1 0x37 
 
-void moveF();
-void moveB();
+void move_step(int s);    // Move amount of steps
+void moveF();   // Move forward
+void moveB();   // Move backwards
+void move_d(int dis); // Move certain distance
+
+#define stpa_motor 1.8  // Step angle of a motor
+#define d 7.89 // One turn moves 79.39mm
+int cycle = (int)360/stpa_motor; // Amount of steps for one turn
+char pos[4] = {0b0001, 0b0100, 0b1000, 0b0010}; // Motor configuration
 
 int main(void) {     
+    uart_init();    // Open the communication to the micro controller
+    io_redirect();  // Redirect input and output to the communication
+
     /* Declaration of I/O Pins */
     DDRD = 0xF0;    // Set Port D as input for the buttons 0b1111 0000
     PORTD = 0x3F;   // Activate internall pull
     DDRB = 0xFF;    // Set Port B as output for the LN298N 0b1111 1111
-
+   
     /* Declare variables */
-    
+    int d_run;
+
     while (1){
-        //button_state = PIND;
         switch (PIND){
         case b0:
-            moveF();
-            _delay_ms(10);
+            printf("\nDistance: ");
+            scanf("%d", &d_run);
+            move_d(d_run);
+            printf("\nDone");
             break;
         
         case b1:
-            moveB();            
+            printf("\nTurns: ");
+            scanf("%d", &d_run);
+            move_step(d_run);
+            printf("\nDone");
             break;
         default:
-            PORTB = 0X00;       // Set all output to 0
+            PORTB = 0X00;   // Set all output to 0
             break;
         }
     }
 }
 
 void moveF(){
-    int i;
-    char pos[4] = {0b0001, 0b0100, 0b1000, 0b0010};
-    for(int j = 0; j < 50 ; j++){
-        for(i = 0; i < 4 ; i++){
-            PORTB = pos[i];
-            _delay_ms(30);
-        }
+    for(int i = 0; i < 4 ; i++){
+        PORTB = pos[i];
+        _delay_ms(4);
     }
 }
 void moveB(){
-    int i;
-    char pos[4] = {0b0001, 0b0100, 0b1000, 0b0010};
-    for(i = 3; i >= 0 ; i--){
+    for(int i = 3; i >= 0 ; i--){
         PORTB = pos[i];
-        _delay_ms(30);
+        _delay_ms(10);
+    }
+}
+void move_step(int s){
+    int turns = 50*s;
+    for(int j = 0; j < turns ; j++){
+        for(int i = 0; i < 4 ; i++){
+            PORTB = pos[i];
+            _delay_ms(4);
+        }
+    }
+}
+
+void move_d(int dis){
+    int distance = dis*50/d;
+    for(int j = 0; j < distance ; j++){moveF();
+        /*for(int i = 0; i < 4 ; i++){
+            PORTB = pos[i];
+            _delay_ms(4);
+        }*/
     }
 }
