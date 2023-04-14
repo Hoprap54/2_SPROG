042d8517b3689307655e6391cfc9220f68965e3d
Hoprap <benitopmwk@gmail.com>
2023-04-14 10:23:04 +0200

    Save functions in subfunction c file

--- src/main.c
+++ src/main.c
@@ -18,15 +18,6 @@
 #define b0 0x3b
 #define b1 0x37 
 
-void move_step(int s);    // Move amount of steps
-void moveF();   // Move forward
-void moveB();   // Move backwards
-void move_d(int dis); // Move certain distance
-
-#define stpa_motor 1.8  // Step angle of a motor
-#define d 7.89 // One turn moves 79.39mm
-int cycle = (int)360/stpa_motor; // Amount of steps for one turn
-char pos[4] = {0b0001, 0b0100, 0b1000, 0b0010}; // Motor configuration
 
 int main(void) {     
     uart_init();    // Open the communication to the micro controller
@@ -50,40 +41,17 @@ int main(void) {
             break;
         
         case b1:
+        /*
             printf("\nTurns: ");
             scanf("%d", &d_run);
             move_step(d_run);
             printf("\nDone");
+        */
+            moveB();
             break;
         default:
             PORTB = 0X00;   // Set all output to 0
             break;
         }
-    }
-}
-
-void moveF(){
-    for(int i = 0; i < 4 ; i++){
-        PORTB = pos[i];
-        _delay_ms(4);
-    }
-}
-void moveB(){
-    for(int i = 3; i >= 0 ; i--){
-        PORTB = pos[i];
-        _delay_ms(10);
-    }
-}
-void move_step(int s){
-    int turns = 50*s;
-    for(int j = 0; j < turns ; j++){
-        moveF();
-    }
-}
-
-void move_d(int dis){
-    int distance = dis*50/d;
-    for(int j = 0; j < distance ; j++){
-        moveF();
     }
 }
--- src/subfunctions.c
+++ src/subfunctions.c
@@ -12,3 +12,33 @@
 #include "usart.h"
 #include "subfunctions.h"
 #include <stdbool.h>
+
+#define stpa_motor 1.8  // Step angle of a motor
+#define d 7.89 // One turn moves 79.39mm
+int cycle = (int)360/stpa_motor; // Amount of steps for one turn
+char pos[4] = {0b0001, 0b0100, 0b1000, 0b0010}; // Motor configuration
+
+void moveF(){
+    for(int i = 0; i < 4 ; i++){
+        PORTB = pos[i];
+        _delay_ms(4);
+    }
+}
+void moveB(){
+    for(int i = 3; i >= 0 ; i--){
+        PORTB = pos[i];
+        _delay_ms(4);
+    }
+}
+void move_step(int s){  // Move amount of steps
+    int turns = 50*s;
+    for(int j = 0; j < turns ; j++){
+        moveF();
+    }
+}
+void move_d(int dis){   // Move certain distance
+    int distance = dis*50/d;
+    for(int j = 0; j < distance ; j++){
+        moveF();
+    }
+}
