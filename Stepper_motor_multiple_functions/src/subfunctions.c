ca9ed0afd63c7d4505b8c125756df54ff725826f
svenmackan <marius.tilea03@gmail.com>
2023-05-14 14:17:21 +0200

    myChanges

--- Stepper_motor_multiple_functions/src/subfunctions.c
+++ Stepper_motor_multiple_functions/src/subfunctions.c
@@ -11,44 +11,78 @@
 #include "subfunctions.h"
 #include <stdbool.h>
 
-#define d 1.25 // One turn moves 1.25mm
+#define d 1.25                                  // One turn moves 1.25mm
 char pos[4] = {0b0001, 0b0100, 0b0010, 0b1000}; // Motor configuration
 
-void init_timer0(void){
-    TCNT0 = 0;      // Reset counter
-    OCR0A = 249;    // Set compare register A
+void init_timer0(void)
+{
+    TCNT0 = 0;                           // Reset counter
+    OCR0A = 249;                         // Set compare register A
     TCCR0A |= (1 << WGM01);              // Timer Mode to CTC
     TCCR0B |= (1 << CS01) | (1 << CS00); // Set prescaler to 64
 }
-void delay_ms(unsigned int t_ms){
+void delay_ms(unsigned int t_ms)
+{
     init_timer0();
-    for(unsigned int i = 0; i < t_ms; i++){
-    // Compare count with delay value needed
-        while ((TIFR0 & (1 << OCF0A)) == 0){} // Wait for the overflow eventº
-        TIFR0 = (1 << OCF0A);                 // Set timer to start counting again
+    for (unsigned int i = 0; i < t_ms; i++)
+    {
+        // Compare count with delay value needed
+        while ((TIFR0 & (1 << OCF0A)) == 0)
+        {
+        }                     // Wait for the overflow eventº
+        TIFR0 = (1 << OCF0A); // Set timer to start counting again
     }
 }
-void move_F_PB(){
-    for(int i = 0; i < 4 ; i++){
+void move_F_PB()
+{
+    for (int i = 0; i < 4; i++)
+    {
         PORTB = pos[i];
         delay_ms(3);
     }
 }
-void move_B_PB(){
-    for(int i = 3; i >= 0 ; i--){
+void move_B_PB()
+{
+    for (int i = 3; i >= 0; i--)
+    {
         PORTB = pos[i];
         delay_ms(3);
     }
 }
-void move_F_PD(){
-    for(int i = 0; i < 4 ; i++){
-        PORTD = pos[i]<<4;
+void move_F_PD()
+{
+    for (int i = 3; i >= 0; i--)
+    {
+        PORTD = pos[i] << 4;
         delay_ms(3);
     }
 }
-void move_B_PD(){
-    for(int i = 3; i >= 0 ; i--){
-        PORTD = pos[i]<<4;
+void move_B_PD()
+{
+    for (int i = 0; i < 4; i++)
+    {
+        PORTD = pos[i] << 4;
+        delay_ms(3);
+    }
+}
+
+void move_same_time_B()
+{
+    int j = 0;
+    for (int i = 3; i >= 0; i--, j++)
+    {
+        PORTD = pos[j] << 4;
+        PORTB = pos[i];
+        delay_ms(3);
+    }
+}
+void move_same_time_F()
+{
+    int j = 3;
+    for (int i = 0; i < 4; i++, j--)
+    {
+        PORTD = pos[j] << 4;
+        PORTB = pos[i];
         delay_ms(3);
     }
 }
